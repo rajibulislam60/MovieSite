5efb1f49faa58b442be936c4dfa65fc5cbb22746
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-15 11:46:16 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className='bg-green-500'>App</div>
-  )
-}
+    <div>
+      <Home />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-blue-950 text-white">
+      <div className="container">
+        <div className="flex gap-10 items-center py-5">
+          <h2 className="text-[24px] font-bold leading-7">Secrect Movie</h2>
+          <div>
+            <ul className="flex gap-3">
+              <li>Movies</li>
+              <li>TV Shows</li>
+              <li>People</li>
+              <li>More</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
