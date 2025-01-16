6609c1b19700ed56f7684313ae40457540fd5a17
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-16 19:15:20 +0600

    add link img and footer part add logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,17 @@ const Footer = () => {
       <div className="container">
         <div className="flex justify-around items-center py-10">
           <div className="text-center">
-            <h2 className="text-[24px] font-bold leading-7">Secret Movie</h2>
-            <button className="bg-white text-blue-950 px-4 py-2 rounded-md mt-4 font-medium">JOIN THE COMMINITY</button>
+            <div className="flex items-center gap-2">
+              <h2 className="text-[24px] font-bold leading-7">Secret Movie</h2>
+              <img
+                className="w-[30px]"
+                src="/src/assets/images/logo.png"
+                alt="Logo"
+              />
+            </div>
+            <button className="bg-white text-blue-950 px-4 py-2 rounded-md mt-4 font-medium">
+              JOIN THE COMMINITY
+            </button>
           </div>
           <div className="flex gap-[120px]">
             <div>
