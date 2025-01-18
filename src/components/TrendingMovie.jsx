bd8290a0f0f997b1e6b078d1d19cf1319054de88
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-18 16:38:40 +0600

    trending area fetch done

--- src/components/TrendingMovie.jsx
+++ src/components/TrendingMovie.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const TrendingMovie = () => {
-  return (
-    <div className='container'>
-        <div>
-            <h2 className='text-[24px] font-bold leading-7 text-blue-950 mt-10'>Trending</h2>
-        </div>
-        <div className='w-[15%] h-[400px] mt-6'>
-            <img className='w-full h-[80%] object-cover' src="/src/assets/images/headerImg.jpg" alt="" />
-            <div>
-                <h3 className='text-[16px] font-semibold'>My tiny senpai</h3>
-                <h4 className='text-[14px] font-normal'>Dec 15, 2024</h4>
-            </div>
-        </div>
-    </div>
-  )
-}
+  let [trendingShows, setTrendingShows] = useState([]);
 
-export default TrendingMovie
+  const getTrendingshows = async () => {
+    try {
+      await fetch(
+        "https://api.themoviedb.org/3/trending/all/day?api_key=23cf808c614383d83cc32bec99a11a97"
+      )
+        .then((res) => res.json())
+        .then((json) => setTrendingShows(json.results));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTrendingshows();
+  }, []);
+  return (
+    <div className="container">
+      <div>
+        <h2 className="text-[24px] font-bold leading-7 text-blue-950 mt-10">
+          Trending
+        </h2>
+      </div>
+      <div className="grid grid-cols-5 gap-4">
+      {trendingShows.map((data) => (
+        <div className="w-full mt-6">
+          <img
+            className="w-full h-[80%] object-cover"
+            src="/src/assets/images/headerImg.jpg"
+            alt=""
+          />
+          <div>
+            <h3 className="text-[16px] font-semibold">My tiny senpai</h3>
+            <h4 className="text-[14px] font-normal">Dec 15, 2024</h4>
+          </div>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrendingMovie;
