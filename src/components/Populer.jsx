9ecbed068442cae3acc00091b987db652cb35f63
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-18 20:19:16 +0600

    populer tv series fetch and design done

--- src/components/Populer.jsx
+++ src/components/Populer.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Populer = () => {
-  return (
-    <div className='container'>
-        <div>
-            <h2 className='text-[24px] font-bold leading-7 text-blue-950 mt-10'>What's Popular</h2>
-        </div>
-        <div className='w-[15%] h-[400px] mt-6'>
-            <img className='w-full h-[80%] object-cover' src="/src/assets/images/headerImg.jpg" alt="" />
-            <div>
-                <h3 className='text-[16px] font-semibold'>My tiny senpai</h3>
-                <h4 className='text-[14px] font-normal'>Dec 15, 2024</h4>
-            </div>
-        </div>
-    </div>
-  )
-}
+  let [popularShows, setPopulerShows] = useState([])
 
-export default Populer
+
+  const getPopulerShows = async()=>{
+    try {
+      await fetch("https://api.themoviedb.org/3/tv/popular?api_key=23cf808c614383d83cc32bec99a11a97")
+      .then(res=>res.json())
+      .then(json=> setPopulerShows(json.results))
+    } catch (error) {
+      console.error(error)
+    }
+    
+  }
+
+  useEffect(()=>{
+    getPopulerShows()
+  }, [])
+  return (
+    <div className="container mb-8">
+      <div>
+        <h2 className="text-[24px] font-bold leading-7 text-blue-950 mt-10">
+          Popular Tv Series
+        </h2>
+      </div>
+      <div className="flex gap-3 overflow-x-auto scrollbar-hide mt-6">
+        {popularShows.map((data) => (
+          <div
+            key={data.id}
+            className="w-[200px] h-[400px] flex-shrink-0 overflow-hidden"
+          >
+            <img
+              className="w-full h-[80%] object-cover"
+              src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+              alt={data.title || data.name || "Trending Show"}
+            />
+            <div>
+              <h3 className="text-[16px] font-semibold">
+                {data.title || data.name || "No Title"}
+              </h3>
+              <h4 className="text-[14px] font-normal">
+                {data.release_date || data.first_air_date || "Unknown Date"}
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Populer;
