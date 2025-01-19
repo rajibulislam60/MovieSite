167784806c4b5fc5b42da27ee7798d276c09c979
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-19 13:45:56 +0600

    movies area fetch done

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Movies = () => {
+    let [movies, setMovies] = useState([])
+
+    const getMovies = async () => {
+        try {
+          await fetch(
+            "https://api.themoviedb.org/3/movie/now_playing?api_key=23cf808c614383d83cc32bec99a11a97"
+          )
+            .then((res) => res.json())
+            .then((json) => setMovies(json.results));
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    
+      useEffect(() => {
+        getMovies();
+      }, []);
   return (
-    <div>Movies</div>
+    <div className='container'>
+        <div>
+        <h2 className="text-[24px] font-bold leading-7 text-blue-950 mt-10">
+          All Movie
+        </h2>
+      </div>
+      <div className="grid grid-cols-5 mt-6">
+        {movies.map((data) => (
+          <div
+            key={data.id}
+            className="w-[200px] h-[400px] flex-shrink-0 overflow-hidden"
+          >
+            <img
+              className="w-full h-[80%] object-cover"
+              src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+              alt={data.title || data.name || "Trending Show"}
+            />
+            <div>
+              <h3 className="text-[16px] font-semibold">
+                {data.title || data.name || "No Title"}
+              </h3>
+              <h4 className="text-[14px] font-normal">
+                {data.release_date || data.first_air_date || "Unknown Date"}
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
